24da631588eb7091a1cffb2584617307706eb400
dobestan <dobestan@gmail.com>
2014-03-18 22:26:13 +0900

    test finish

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def home
     @items = Item.paginate(page: params[:page])
+    @finish_0 = Item.where(finish: 0).count
+    @finish_1 = Item.where(finish: 1).count
+    @deal_0 = Item.where(deal: 0).count
+    @deal_1 = Item.where(deal: 1).count
+    @deal_2 = Item.where(deal: 2).count
   end
 
   def sell
